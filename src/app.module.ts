82bf4fe5bc06055f0a44701a2ff66e42203a78d5
Karim Kompissi <40320510+karimalik@users.noreply.github.com>
2025-08-08 10:53:14 +0000

    Modifie la configuration de TypeORM dans app.module.ts en désactivant la synchronisation de la base de données.

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,7 +26,7 @@ import { Session } from './users/entities/session.entity';
         password: process.env.DB_PASSWORD ?? '',
         database: process.env.DB_NAME ?? '',
         entities: [User, Session, Notification],
-        synchronize: true,
+        synchronize: false,
         logging: process.env.NODE_ENV === 'development',
       }),
     }),
