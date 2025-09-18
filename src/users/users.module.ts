b1b70f30b94e21e47a1c889d3c089d3954b0cf2e
Karim Kompissi <40320510+karimalik@users.noreply.github.com>
2025-09-18 17:19:38 +0000

    Ajout du UploadController et du UploadService dans le module Users pour gérer les fonctionnalités de téléchargement. Mise à jour des contrôleurs et des fournisseurs en conséquence.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,7 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersController } from './users.controller';
+import { UploadController } from './controllers/upload.controller';
 import { UsersService } from './users.service';
+import { UploadService } from './services/upload.service';
 import { User } from './entities/user.entity';
 import { Profile } from './entities/profile.entity';
 import { Session } from './entities/session.entity';
@@ -35,8 +37,8 @@ import { EmailModule } from './email/email.module';
     }),
     EmailModule,
   ],
-  controllers: [UsersController],
-  providers: [UsersService, CustomJwtService, JwtAuthGuard, AdminGuard],
+  controllers: [UsersController, UploadController],
+  providers: [UsersService, UploadService, CustomJwtService, JwtAuthGuard, AdminGuard],
   exports: [UsersService, CustomJwtService, JwtAuthGuard, AdminGuard, TypeOrmModule],
 })
 export class UsersModule { }
